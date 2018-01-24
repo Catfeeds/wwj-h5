e39b8571fdcc098b8fdc06930079be9716e85928
lyyCynthia <Cynthia_lyue@163.com>
2018-01-24 11:16:21 +0800

    delete

--- src/main/java/com/zww/user/controller/Test.java
+++ src/main/java/com/zww/user/controller/Test.java
@@ -12,5 +12,6 @@ public class Test {
         System.out.println("操作GIT,测试");
         System.out.println("修改01.24");
         System.out.println("修改01.24，加入组织！");
+        System.out.println("@$%$%^$##%^");
     }
 }
