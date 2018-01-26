05ef7d3aadad000d207b25d8694cfbe0ffe05d72
kuangweijun <kwj2012@163.com>
2018-01-26 10:59:18 +0800

    首页房间信息一览和LoginToken接口的修改。

--- src/main/java/com/zww/user/controller/LoginTokenController.java
+++ src/main/java/com/zww/user/controller/LoginTokenController.java
@@ -4,11 +4,14 @@ import com.zww.constants.SignConstants;
 import com.zww.user.vo.LoginTokenInputVo;
 import com.zww.user.vo.LoginTokenOutputVo;
 import com.zww.util.UUIDUtil;
+import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 
 /**
@@ -23,7 +26,7 @@ public class LoginTokenController {
 
     @ResponseBody
     @RequestMapping("/loginToken")
-    public LoginTokenOutputVo loginToken(@RequestBody LoginTokenInputVo loginTokenInputVo) {
+    public String loginToken(@RequestBody LoginTokenInputVo loginTokenInputVo) {
 
         String appKey = "0xac,0x71,0x8a,0x2a,0x91,0x1d,0x98,0x41,0x6b,0xaf,0x60,0x8f,0xa8,0x4d,0xf8,0x09,0x5c,0x7a,0xe6,0xcb,0x53,0xd6,0xc0,0xcb,0x4e,0xdd,0x5e,0x8b,0xab,0x3a,0x96,0xea";
 
@@ -43,7 +46,11 @@ public class LoginTokenController {
         result.setNonce(nonce);
         result.setExpired(expired);
 
-        return result;
+        // base64加密
+        JSONObject json = JSONObject.fromObject(result);
+        String str = json.toString();
+
+        return getBase64(str);
     }
 
     //生成MD5
@@ -75,5 +82,24 @@ public class LoginTokenController {
             hexStr.append(Integer.toHexString(num));
         }
         return hexStr.toString().toUpperCase();
+    }
+
+    // base64加密
+    public static String getBase64(String str){
+        byte[] b=null;
+        String s=null;
+        try {
+            b = str.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if(b!=null){
+            try {
+                s=new String(Base64.encodeBase64(b),"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return s;
     }
 }
