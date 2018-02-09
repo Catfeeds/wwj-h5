46d5e990ca855cb05c89dac2f835bcd83433f827
kuangweijun <kwj2012@163.com>
2018-02-09 11:38:00 +0800

    回调函数的修改

--- src/main/java/com/zww/callback/controller/WwjliveController.java
+++ src/main/java/com/zww/callback/controller/WwjliveController.java
@@ -8,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 即构回调接口
@@ -28,27 +29,26 @@ public class WwjliveController {
 
     /**
      * 流创建回调接口
-     * @param request
+     * @param
      * @return
      */
     @ResponseBody
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public int create(HttpServletRequest request) {
-        String id = request.getParameter("id");
-        String live_id = request.getParameter("live_id");
-        String channel_id = request.getParameter("channel_id");
-        String title = request.getParameter("title");
-        String stream_alias = request.getParameter("stream_alias");
-        String publish_id = request.getParameter("publish_id");
-        String publish_name = request.getParameter("publish_name");
-        String[] rtmp_url = request.getParameterValues("rtmp_url[]");
-        String[] hls_url = request.getParameterValues("hls_url[]");
-        String[] hdl_url = request.getParameterValues("hdl_url[]");
-        String[] pic_url = request.getParameterValues("pic_url[]");
-        String create_time = request.getParameter("create_time");
-        String timestamp = request.getParameter("timestamp");
-        String nonce = request.getParameter("nonce");
-        String signature = request.getParameter("signature");
+    public int create(@RequestParam("id") String id,
+                      @RequestParam("live_id") String live_id,
+                      @RequestParam("channel_id") String channel_id,
+                      @RequestParam("title") String title,
+                      @RequestParam("stream_alias") String stream_alias,
+                      @RequestParam("publish_id") String publish_id,
+                      @RequestParam("publish_name") String publish_name,
+                      @RequestParam("rtmp_url[]") List<String> rtmp_url,
+                      @RequestParam("hls_url[]") List<String> hls_url,
+                      @RequestParam("hdl_url[]") List<String> hdl_url,
+                      @RequestParam("pic_url[]") List<String> pic_url,
+                      @RequestParam("create_time") String create_time,
+                      @RequestParam("timestamp") String timestamp,
+                      @RequestParam("nonce") String nonce,
+                      @RequestParam("signature") String signature) {
 
         // 设置追加记录内容
         StreamCreateInfoPojo pojo = new StreamCreateInfoPojo();
@@ -59,20 +59,20 @@ public class WwjliveController {
         pojo.setStream_alias(stream_alias);
         pojo.setPublish_id(publish_id);
         pojo.setPublish_name(publish_name);
-        if (rtmp_url.length >= 2) {
-            pojo.setRtmp_url_0(rtmp_url[0]);
-            pojo.setRtmp_url_1(rtmp_url[1]);
+        if (rtmp_url.size() >= 2) {
+            pojo.setRtmp_url_0(rtmp_url.get(0));
+            pojo.setRtmp_url_1(rtmp_url.get(1));
         }
-        if (hls_url.length >= 2) {
-            pojo.setHls_url_0(hls_url[0]);
-            pojo.setHls_url_1(hls_url[1]);
+        if (hls_url.size() >= 2) {
+            pojo.setHls_url_0(hls_url.get(0));
+            pojo.setHls_url_1(hls_url.get(1));
         }
-        if (hdl_url.length >= 2) {
-            pojo.setHdl_url_0(hdl_url[0]);
-            pojo.setHdl_url_1(hdl_url[1]);
+        if (hdl_url.size() >= 2) {
+            pojo.setHdl_url_0(hdl_url.get(0));
+            pojo.setHdl_url_1(hdl_url.get(1));
         }
-        if (pic_url.length >= 1) {
-            pojo.setPic_url_0(pic_url[0]);
+        if (pic_url.size() >= 1) {
+            pojo.setPic_url_0(pic_url.get(0));
         }
         pojo.setCreate_time_zego(StringUtils.isEmpty(create_time) ? 0 : Long.valueOf(create_time));
         pojo.setTimestamp_zego(StringUtils.isEmpty(timestamp) ? 0 : Long.valueOf(timestamp));
@@ -85,20 +85,19 @@ public class WwjliveController {
 
     /**
      * 流关闭回调接口
-     * @param request
+     * @param
      * @return
      */
     @ResponseBody
     @RequestMapping(value = "/close", method = RequestMethod.POST)
-    public int close(HttpServletRequest request) {
-        String id = request.getParameter("id");
-        String type = request.getParameter("type");
-        String channel_id = request.getParameter("channel_id");
-        String stream_alias = request.getParameter("stream_alias");
-        String third_define_data = request.getParameter("third_define_data");
-        String timestamp = request.getParameter("timestamp");
-        String nonce = request.getParameter("nonce");
-        String signature = request.getParameter("signature");
+    public int close(@RequestParam("id") String id,
+                     @RequestParam("type") String type,
+                     @RequestParam("channel_id") String channel_id,
+                     @RequestParam("stream_alias") String stream_alias,
+                     @RequestParam("third_define_data") String third_define_data,
+                     @RequestParam("timestamp") String timestamp,
+                     @RequestParam("nonce") String nonce,
+                     @RequestParam("signature") String signature) {
 
         // 设置追加记录内容
         StreamCloseInfoPojo pojo = new StreamCloseInfoPojo();
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
+<%@ page session="false" pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
 <html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="refresh"content="0; url=/zww/index.html">
+</head>
 <body>
-<h2>Hello World!</h2>
+<%--<h2>Hello World!</h2>--%>
 </body>
 </html>
